cda69103dbd83f41696d40a70108b772c516cfb1
KoshiNoLimit <zmsh04@gmail.com>
2021-04-30 18:21:14 +0300

    beautify

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
 from mvlog import Mvlog
 from logic import Logic
-from brute_force import *
+from brute_force import compare_logics
 import itertools
 import os
+from sys import argv
 
 
 def parse(path):
+    """Получение логики из mvlog-файла"""
     mvlog = Mvlog(path)
     return Logic(mvlog)
 
 
-path = 'data/LevinMikenberg/'
-files = tuple(map(lambda x: path + x, os.listdir(path)))
-
-logics = [parse(file) for file in files]
-
-# print(*logics, sep='\n\n')
+def get_logics(path):
+    """Получение всех логик из директории"""
+    files = tuple(map(lambda x: path + x, os.listdir(path)))
+    return [parse(file) for file in files]
 
 
-from time import perf_counter
+def algorithm(path):
+    """Основной алгоритм"""
+    logics = get_logics(path)
 
-start = perf_counter()
-for l1, l2 in itertools.combinations(logics, 2):
-    print(l1.name, l2.name, compare_logics(l1, l2))
-print(perf_counter()-start)
+    for l1, l2 in itertools.combinations(logics, 2):
+        yield l1.name, l2.name, compare_logics(l1, l2)
+
+
+if __name__ == '__main__':
+    for res in algorithm(argv[1]):
+        print(res[0], res[1], ':', res[2])
+
